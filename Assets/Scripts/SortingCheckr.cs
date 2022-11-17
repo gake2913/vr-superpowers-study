4027745536edfcc0d0c48a775e1145dc050d033b
gabriel.kempken <64098217+gake2913@users.noreply.github.com>
2022-11-17 20:23:30 +0100

    Add Feedback for Sorting Task

---
+++ Assets/Scripts/SortingCheckr.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingCheckr : MonoBehaviour
+{
+
+    public static List<GameObject> SortedBalls = new List<GameObject>();
+    public static int CorrectSorts = 0;
+    public static int WrongSorts = 0;
+
+    public LayerMask AllSortingLayers;
+    public LayerMask CorrectLayer;
+
+    public Animator Lightbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckCollision(Collider collision)
+    {
+        if ((CorrectLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            // correct Ball inserted
+            if (!SortedBalls.Contains(collision.gameObject))
+            {
+                SortedBalls.Add(collision.gameObject);
+                Lightbar.SetTrigger("Pulse_Green");
+                CorrectSorts++;
+                Debug.Log("Ball sorted correctly. Correct Sorts: " + CorrectSorts + ". Wrong Sorts: " + WrongSorts);
+            }
+        }
+        else
+        {
+            // wrong Ball inserted
+            Lightbar.SetTrigger("Pulse_Red");
+            WrongSorts++;
+            Debug.Log("Ball not sorted correctly. Correct Sorts: " + CorrectSorts + ". Wrong Sorts: " + WrongSorts);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //Debug.Log("Collision");
+
+        if ((AllSortingLayers.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            CheckCollision(collision);
+        }
+    }
+}
