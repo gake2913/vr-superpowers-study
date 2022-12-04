d6e094077c0e26ce37dd6166186d7a796d9fba9f
gabriel.kempken <64098217+gake2913@users.noreply.github.com>
2022-12-04 15:10:40 +0100

    See Through Power Fixes

--- Assets/Scripts/SeeThroughPowerActivator.cs
+++ Assets/Scripts/SeeThroughPowerActivator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerActivator : MonoBehaviour
+public class SeeThroughPowerActivator : MonoBehaviour
 {
 
     public Transform Camera;
