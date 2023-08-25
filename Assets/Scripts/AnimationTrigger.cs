64f574338200d342b6762b8790a19e0fc91b2ad0
gabriel.kempken <64098217+gake2913@users.noreply.github.com>
2023-08-25 17:47:53 +0200

    Changed Course completed display

---
+++ Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerAnimation(string trigger)
+    {
+        GetComponent<Animator>().SetTrigger(trigger);
+    }
+
+}
--- Assets/Scripts/ColliderLogger.cs
+++ Assets/Scripts/ColliderLogger.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ColliderLogger : MonoBehaviour
 {
+
+    public UnityEvent OnPlayerEntered = new UnityEvent();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +26,8 @@ public class ColliderLogger : MonoBehaviour
         {
             Logger.CreateLog("### Player has entered the Ending Room. ###");
             StudyLevelManagement.stopTimer();
+
+            OnPlayerEntered.Invoke();
         }
     }
 }
